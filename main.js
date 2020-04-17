80a1d99492e92bce0e862ae34542c4f63698000d
Olga Rozova <olya.smolina@gmail.com>
2020-04-17 23:15:48 +0300

    transform text and insert to page

--- main.js
+++ main.js
@@ -1,44 +1,22 @@
-var num1 = prompt('Enter a first number:');
+function transformStrToHtmlList(str,symb1,symb2){
+    var list = str.split(symb1,1);         
 
-if(num1 === ''){
-    alert('The field is empty');
-}else{
-    if(isFinite(num1)){
-        var num2 = prompt('Enter a second number:');        
-
-        if(num2  === ''){
-            alert('The field is empty');
-        }else{
-            if(isFinite(num2)){
-                var op = prompt('Enter an operator (+ - * /):');
-                console.log(op);
-
-                if(op === '+'){                    
-                    alert('Your result is: '+ (+num1 + +num2));               
-                }else if(op === '-'){
-                    alert('Your result is: '+ (+num1 - +num2));               
-                }else if(op === '*'){
-                    alert('Your result is: '+ (+num1 * +num2));     
-                }else if(op === '/'){        
-                    var res = +num1 / +num2;                            
-
-                    if(isNaN(res)){
-                        alert('Your result is not a number');
-                    }else if(res === Infinity || res === -Infinity){
-                        alert('Your result is an infinity');                         
-                    }else{
-                        alert('Your result is: '+ res);                         
-                    }
-                }else{
-                    alert('An invalid operator'); 
-                }
-                
-            }else{        
-                alert('You should enter a number');
-            }            
-        }
-                    
-    }else{    
-        alert('You should enter a number');
+    var pattern = str.slice(list[0].length+1,str.length);    
+    
+    var patternTag = pattern.split(symb2); 
+    
+    list = list[0].split(',');
+           
+    var result = '';
+    for (var item of list){        
+        result += patternTag[0] + item + patternTag[1];                        
     }
+
+    return result;
+}
+
+function insertHtml(){ 
+    var str = document.getElementById("form_transform__text").value;      
+    var html = transformStrToHtmlList(str,'/','$');  
+    document.getElementById('list').innerHTML = html;    
 }
