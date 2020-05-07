ff107c993510300fe3e15826eeb86692ec2231ab
Olga Rozova <olya.smolina@gmail.com>
2020-05-07 19:14:56 +0300

    task 10

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
 
 function init() {
 
-    const arrows = document.querySelectorAll(".arrow");
-
-    for (let arrow of arrows) {
-        arrow.onclick = blockMove;
+    let checkboxes = document.querySelectorAll('.checkbox');
+    for(let checkbox of checkboxes){
+        checkbox.onclick = setActive;        
     }
-
-    function getPosition(obj) {
-        const idParts = obj.id.split('-');
-        return idParts[1];
-
-    }
-
-    function blockMove() {
-
-        const position = getPosition(this);
-        this.parentNode.classList.add(position);
-
-        switch (position) {
-            case 'top':
-                this.parentNode.style.top = this.parentNode.clientHeight / 2 + 'px';
-                break;
-            case 'bottom':
-                this.parentNode.style.top = `calc(100% - ${this.parentNode.clientHeight / 2}px)`;
-                break;
-            case 'left':
-                this.parentNode.style.left = this.parentNode.clientWidth / 2 + 'px';
-                break;
-            case 'right':
-                this.parentNode.style.left = `calc(100% - ${this.parentNode.clientWidth / 2}px)`;
-                break;
-        }
+    function setActive(){        
+        this.classList.toggle('active');        
     }
 
 }
