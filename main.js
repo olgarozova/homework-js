9be7da4f8b0bb176f7bc72461c4456ba1a434521
Olga Rozova <olya.smolina@gmail.com>
2020-05-08 01:01:28 +0300

    task 10

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 
+'use strict'
 function init() {
 
-    let checkboxes = document.querySelectorAll('.checkbox');
-    for(let checkbox of checkboxes){
-        checkbox.onclick = setActive;        
-    }
-    function setActive(){        
-        this.classList.toggle('active');        
-    }
+    var arrow = document.querySelector('#content__menu-arrow');
+    var menuContainer = document.querySelector('#content__menu-wrapper');
+    let menu = document.querySelector('#content__menu');    
 
+    arrow.onclick = showMenu;
+
+    function showMenu() {                        
+        var isShow = this.parentNode.classList.toggle('show');
+        if(isShow){
+            menuContainer.style.paddingBottom = (menu.offsetHeight + arrow.offsetHeight) + 'px';    
+            arrow.style.top = menu.offsetHeight + 'px';
+        }else{
+            menuContainer.style.paddingBottom = arrow.offsetHeight + 'px';    
+            arrow.style.top = 0;
+        }     
+    }
 }
 init();
 
