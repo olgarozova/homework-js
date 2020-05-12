a52ab0759386278234f0d0d2caa222019381b0bf
Olga Rozova <olya.smolina@gmail.com>
2020-05-12 20:00:29 +0300

    task 14

--- main.js
+++ main.js
@@ -2,22 +2,70 @@
 'use strict'
 function init() {
 
-    var arrow = document.querySelector('#content__menu-arrow');
-    var menuContainer = document.querySelector('#content__menu-wrapper');
-    let menu = document.querySelector('#content__menu');    
+    let inputElement = document.querySelector('#user-input');
+    let message = document.querySelector('#user-input-message');
+    let selectElement = document.querySelector('#input-type');    
 
-    arrow.onclick = showMenu;
+    inputElement.onfocus = validation;
+    inputElement.oninput = validation;
+    inputElement.onblur = stopValidation;
+    selectElement.onchange = validation;
+    
 
-    function showMenu() {                        
-        var isShow = this.parentNode.classList.toggle('show');
-        if(isShow){
-            menuContainer.style.paddingBottom = (menu.offsetHeight + arrow.offsetHeight) + 'px';    
-            arrow.style.top = menu.offsetHeight + 'px';
-        }else{
-            menuContainer.style.paddingBottom = arrow.offsetHeight + 'px';    
-            arrow.style.top = 0;
-        }     
+    function isValid(type,str){
+
+        message.innerHTML = "";
+        message.classList.remove('show');
+        inputElement.classList.remove('input-error');
+
+        if(str){
+            inputElement.classList.add('input-ok');
+        }
+
+        for (let char of str) {                
+            let symbol = char.toLowerCase();
+
+            let isNumber = symbol.charCodeAt() >= 48 && symbol.charCodeAt() <= 57;        
+            let isLetter = symbol.charCodeAt() >= 97 && symbol.charCodeAt() <= 122;
+            let isComma = symbol === ',';  
+            let isDot = symbol === '.';                
+
+            if(type === 'lndc' && (!isLetter && !isNumber && !isComma && !isDot)) return false;                                
+            if(type === 'letter' && !isLetter) return false;                                
+            if(type === 'number' && !isNumber) return false;                                
+  
+        }
+        return true;
+    }  
+
+    function validation(){ 
+        
+        message.innerHTML = "";
+        message.classList.remove('show');
+        inputElement.classList.remove('input-error');    
+
+        if(selectElement.value === 'none' && inputElement.value !==''){
+            message.classList.add('show');
+            message.innerHTML = selectElement[selectElement.selectedIndex].label;
+            return;
+        }
+        
+        if(!isValid(selectElement.value,inputElement.value)){
+            inputElement.classList.add('input-error');
+            inputElement.classList.remove('input-ok');
+            message.classList.add('show');
+            message.innerHTML = "Allow " + selectElement[selectElement.selectedIndex].label.toLowerCase();       
+        } 
+
     }
+
+    function stopValidation(){
+
+        message.classList.remove('show');
+        inputElement.classList.remove('input-error');
+        inputElement.classList.remove('input-ok');
+    }
+
 }
 init();
 
