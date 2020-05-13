c5f70f39fc68239a437a75a5a289737b0aa32387
Olga Rozova <olya.smolina@gmail.com>
2020-05-13 13:54:24 +0300

    form animation

--- main.js
+++ main.js
@@ -1,72 +1,22 @@
-
 'use strict'
+function formAction(){        
+    
+    let modal = document.querySelector('#modal');
+    let overlay = document.querySelector('#overlay');
+
+    modal.classList.toggle('show');
+    overlay.classList.toggle('show');
+}
+
 function init() {
 
-    let inputElement = document.querySelector('#user-input');
-    let message = document.querySelector('#user-input-message');
-    let selectElement = document.querySelector('#input-type');    
-
-    inputElement.onfocus = validation;
-    inputElement.oninput = validation;
-    inputElement.onblur = stopValidation;
-    selectElement.onchange = validation;
-    
-
-    function isValid(type,str){
-
-        message.innerHTML = "";
-        message.classList.remove('show');
-        inputElement.classList.remove('input-error');
-
-        if(str){
-            inputElement.classList.add('input-ok');
-        }
-
-        for (let char of str) {                
-            let symbol = char.toLowerCase();
-
-            let isNumber = symbol.charCodeAt() >= 48 && symbol.charCodeAt() <= 57;        
-            let isLetter = symbol.charCodeAt() >= 97 && symbol.charCodeAt() <= 122;
-            let isComma = symbol === ',';  
-            let isDot = symbol === '.';                
-
-            if(type === 'lndc' && (!isLetter && !isNumber && !isComma && !isDot)) return false;                                
-            if(type === 'letter' && !isLetter) return false;                                
-            if(type === 'number' && !isNumber) return false;                                
-  
-        }
-        return true;
-    }  
-
-    function validation(){ 
+    let buttonSubscribe = document.querySelector('#button-subscribe');
+    buttonSubscribe.onclick = formAction;
+    let buttonSend = document.querySelector('#button-send');
+    buttonSend.onclick = formAction;
+    let buttonClose = document.querySelector('#button-close');
+    buttonClose.onclick = formAction;
         
-        message.innerHTML = "";
-        message.classList.remove('show');
-        inputElement.classList.remove('input-error');  
-        inputElement.classList.remove('input-ok');  
-
-        if(selectElement.value === 'none' && inputElement.value !==''){
-            message.classList.add('show');
-            message.innerHTML = selectElement[selectElement.selectedIndex].label;
-            return;
-        }
-        
-        if(!isValid(selectElement.value,inputElement.value)){
-            inputElement.classList.add('input-error');
-            inputElement.classList.remove('input-ok');
-            message.classList.add('show');
-            message.innerHTML = "Allow " + selectElement[selectElement.selectedIndex].label.toLowerCase();       
-        } 
-
-    }
-
-    function stopValidation(){
-      
-        if(inputElement.classList.contains('input-ok')){            
-            inputElement.classList.remove('input-ok');
-        }
-    }
-
 }
 init();
 
